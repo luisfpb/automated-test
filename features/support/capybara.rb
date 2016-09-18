7df392a1a1eddc6dd8f86eb62d0aec7705758629
Luis Benassi <luis.benassi@walmart.com>
2016-09-18 20:46:28 -0300

    Capybara configuration for chrome

---
+++ features/support/capybara.rb
@@ -0,0 +1,13 @@
+case $driver
+when 'firefox'
+else
+	$capybara_driver = :chrome
+
+	Capybara.register_driver :chrome do |app|
+		caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--start-maximized" ]})
+		Capybara::Selenium::Driver.new(app, {:browser => :chrome, :desired_capabilities => caps})
+	end
+end
+
+Capybara.default_driver = $capybara_driver
+Capybara.default_max_wait_time = 15
