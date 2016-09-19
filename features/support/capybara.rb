9ff77572dc9aa9952c4f2f7324683fc55d1d5ac7
Luis Benassi <luis.benassi@walmart.com>
2016-09-19 03:31:17 -0300

    capybara configuration and delete helper file

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -12,3 +12,18 @@ end
 
 Capybara.default_driver = $capybara_driver
 Capybara.default_max_wait_time = 15
+
+module CapybaraExtension
+  def drag_by(right_by, down_by)
+    base.drag_by(right_by, down_by)
+  end
+end
+
+module CapybaraSeleniumExtension
+  def drag_by(right_by, down_by)
+    driver.browser.action.drag_and_drop_by(native, right_by, down_by).perform
+  end
+end
+
+::Capybara::Selenium::Node.send :include, CapybaraSeleniumExtension
+::Capybara::Node::Element.send :include, CapybaraExtension
